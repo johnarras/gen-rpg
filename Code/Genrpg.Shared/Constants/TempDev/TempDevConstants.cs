27565f970fc73a9d526f3c91f67554bcde0abd1d
johnarras <johnarras@gmail.com>
2023-11-04 14:12:39 -0500

    change TempDevConstants to have empty urls

--- Code/Genrpg.Shared/Constants/TempDev/TempDevConstants.cs
+++ Code/Genrpg.Shared/Constants/TempDev/TempDevConstants.cs
@@ -6,14 +6,14 @@ namespace Genrpg.Shared.Constants.TempDev
 {
     public class TempDevConstants
     {
-        public const string ConfigEndpoint = "https://genrpgconfig.azurewebsites.net/api/GenrpgConfig";
+        public const string ConfigEndpoint = "https://addyourconfigendpoint.azurewebsites.net/api/GenrpgConfig";
 
         public const string LocalIP = "127.0.0.1";
         public const string TestVMIP = "20.83.41.163";
 
         public const int StartPort = 4000;
 
-        public const string DefaultAssetPrefix = "http://oxdbcontent.blob.core.windows.net/genrpgdev/";
+        public const string DefaultAssetPrefix = "http://addyourcontentendpoint.blob.core.windows.net/genrpgdev/";
         
     }
 }
